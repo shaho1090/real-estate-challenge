77b1f0d2a10f054442ea42a46ce72be990bbfea8
shaho1090 <shaho.parvini@gmail.com>
2021-08-25 01:17:24 +0430

    add jwt classes to aliases

--- config/app.php
+++ config/app.php
@@ -233,6 +233,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
     ],
 
 ];
