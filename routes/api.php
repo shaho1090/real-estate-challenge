83145d42c9d3270cf3d9cd46057b32335f57fabe
shaho1090 <shaho.parvini@gmail.com>
2021-08-25 01:16:27 +0430

    ignore sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+require __DIR__.'/auth.php';
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
