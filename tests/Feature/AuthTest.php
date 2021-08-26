c48fe76f4869491e46b50729503fb9a656cff82e
shaho1090 <shaho.parvini@gmail.com>
2021-08-26 23:25:09 +0430

    add faker to testCase

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -6,13 +6,12 @@ use App\Models\User;
 use App\Models\UserType;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    use RefreshDatabase, withFaker;
+    use RefreshDatabase;
 
     /**
      * @var array
