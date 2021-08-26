35010dbc26665942b66b9c2d6720d146a8a7ddd2
shaho1090 <shaho.parvini@gmail.com>
2021-08-26 23:25:39 +0430

    create sign in method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, withFaker;
+
+    public function signIn()
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'family' => $this->faker->lastName,
+            'phone' => $this->faker->phoneNumber,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+            'address' => $this->faker->address,
+        ];
+
+        $this->postJson(route('register'), $userData);
+
+        $this->postJson(route('login'),[
+            'email' => $userData['email'],
+            'password' => $userData['password']
+        ]);
+    }
 }
