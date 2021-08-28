278014a29eb14613bb6022db473f888fefd184dc
shaho1090 <shaho.parvini@gmail.com>
2021-08-28 22:15:51 +0430

    refactoring on home routes

--- routes/home.php
+++ routes/home.php
@@ -1,18 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Customer\MyHomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/homes/create', [HomeController::class, 'store'])
-    ->middleware(['api'])
-    ->name('create-home');
-
-Route::get('/homes', [HomeController::class, 'index'])
-    ->middleware(['api'])
-    ->name('homes');
-
-//Route::middleware(['api'])->prefix('auth')->group(function () {
-//    Route::post('logout', [AuthController::class, 'logout']);
-//    Route::post('refresh', [AuthController::class, 'refresh']);
-//    Route::post('me', [AuthController::class, 'me']);
-//});
+Route::middleware(['api'])->prefix('/customer')->group(function () {
+    Route::post('/homes/create', [MyHomeController::class, 'store'])->name('customer-home-create');
+    Route::get('/homes', [MyHomeController::class, 'index'])->name('customer-home-index');
+});
