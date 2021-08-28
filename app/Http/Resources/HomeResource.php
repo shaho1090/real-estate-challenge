3341a83ad1cc899c8f5ce878c2b1f615d9a996b9
shaho1090 <shaho.parvini@gmail.com>
2021-08-28 13:19:33 +0430

    create resource fo home and user

---
+++ app/Http/Resources/HomeResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HomeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'title'=> $this->title,
+            'purpose'=> $this->purpose,
+            'zip_code'=> $this->zip_code,
+            'address'=> $this->address,
+            'price'=> $this->price,
+            'bedrooms'=> $this->bedrooms,
+            'bathrooms'=> $this->bathrooms,
+            'm_two'=> $this->m_two,
+            'price_m_two' => $this->price_m_two,
+            'type' => $this->type->title,
+            'condition' => $this->condition->title
+        ];
+    }
+}
