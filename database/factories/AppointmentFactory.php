217bf3325d80d1ce1467be1aa281abbb9572d3fc
shaho1090 <shaho.parvini@gmail.com>
2021-08-28 22:14:25 +0430

    create appointment factory

---
+++ database/factories/AppointmentFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Appointment;
+use App\Models\Home;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class AppointmentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Appointment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'employee_id' => User::factory()->employee()->create()->id,
+            'home_id' => Home::factory()->create()->id,
+            'date' => Carbon::now()->toDateTimeString(),
+            'start_time' => null,
+            'end_time' => null
+        ];
+    }
+}
