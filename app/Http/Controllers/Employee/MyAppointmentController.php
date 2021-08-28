138fb7b8a639464a75f17fb93a0321c3b4754423
shaho1090 <shaho.parvini@gmail.com>
2021-08-28 22:09:37 +0430

    some refactoring on home and appointment

---
+++ app/Http/Controllers/Employee/MyAppointmentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Employee;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AppointmentCollection;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class AppointmentController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $appointments = auth()->user()->getAppointments();
+
+        return response()->json([
+            'success' => true,
+            'data' => new AppointmentCollection($appointments)
+        ], Response::HTTP_OK);
+    }
+
+}
