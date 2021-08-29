7150158556fa34dc988677536520b82f89b1a4b6
shaho1090 <shaho.parvini@gmail.com>
2021-08-29 14:00:18 +0430

    seperate lanlord and customer routes in auth file

--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,25 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Customer\CustomerRegisterController;
+use App\Http\Controllers\Landlord\LandlordRegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])
     ->middleware('guest')
     ->name('login');
 
-Route::post('/register', [AuthController::class, 'register'])
-    ->name('register');
+Route::post('/landlord-register', [LandlordRegisterController::class, 'register'])
+    ->name('landlord-register');
+
+Route::post('/customer-register', [CustomerRegisterController::class, 'register'])
+    ->name('customer-register');
+
+//Route::middleware(['api'])->prefix('auth')->group(function () {
+//    Route::post('logout', [AuthController::class, 'logout']);
+//    Route::post('refresh', [AuthController::class, 'refresh']);
+//    Route::post('me', [AuthController::class, 'me']);
+//});
 
 //Route::middleware(['api'])->prefix('auth')->group(function () {
 //    Route::post('logout', [AuthController::class, 'logout']);
