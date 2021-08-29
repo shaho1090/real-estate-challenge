7cbae5745b77b483bce0bf1e840432698766d0d2
shaho1090 <shaho.parvini@gmail.com>
2021-08-29 13:58:44 +0430

    remove signIn method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,22 +9,4 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, withFaker;
 
-    public function signIn()
-    {
-        $userData = [
-            'name' => $this->faker->name,
-            'family' => $this->faker->lastName,
-            'phone' => $this->faker->phoneNumber,
-            'email' => $this->faker->email,
-            'password' => $this->faker->password,
-            'address' => $this->faker->address,
-        ];
-
-        $this->postJson(route('register'), $userData);
-
-        $this->postJson(route('login'),[
-            'email' => $userData['email'],
-            'password' => $userData['password']
-        ]);
-    }
 }
