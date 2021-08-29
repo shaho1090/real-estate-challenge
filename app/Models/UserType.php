7475b8ab2bc6612abdcbb1385cf793721f63ee84
shaho1090 <shaho.parvini@gmail.com>
2021-08-29 14:00:44 +0430

    change the way of getting user types

--- app/Models/UserType.php
+++ app/Models/UserType.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Exception;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -17,13 +19,42 @@ class UserType extends Model
         return $this->hasMany(User::class,'type_id');
     }
 
+    /**
+     * @return Builder|Model|object|null
+     * @throws Exception
+     */
     public static function landlord()
     {
-        return self::query()->where('title','landlord')->first();
+        return self::getUserType('landlord');
     }
 
+    /**
+     * @throws Exception
+     */
+    public static function customer()
+    {
+        return self::getUserType('customer');
+    }
+
+    /**
+     * @throws Exception
+     */
     public static function employee()
     {
-        return self::query()->where('title','employee')->first();
+        return self::getUserType('employee');
+    }
+
+    /**
+     * @param string $type
+     * @return Builder|Model|object|null
+     * @throws Exception
+     */
+    private static function getUserType(string $type)
+    {
+        if(!$type = self::query()->where('title',$type)->first()){
+            throw new Exception('The '.$type.' user type was not found!');
+        }
+
+        return $type;
     }
 }
