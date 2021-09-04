d01b317dbf1ea7c1041e93e759db66c3497c3e17
shaho1090 <shaho.parvini@gmail.com>
2021-09-04 12:19:41 +0430

    for using real estate office location

---
+++ app/RealEstateOffice.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App;
+
+
+
+class RealEstateOffice
+{
+    const ZIPCODE = 'cm27pj';
+
+    public static function zipCode(): string
+    {
+        return self::ZIPCODE;
+    }
+}
