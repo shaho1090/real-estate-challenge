f0251bb63c75a5a7212c75f171e7e74a46268093
shaho1090 <shaho.parvini@gmail.com>
2021-09-04 11:48:16 +0430

    using Postcode website by this configuration

---
+++ config/postcodes.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is for accessing postcodes website api
+ * in order to convert zip code to lat long coordinate.
+ *
+ */
+
+return [
+
+    /** GET Method */
+    'address' => 'https://api.postcodes.io/postcodes/'
+];
