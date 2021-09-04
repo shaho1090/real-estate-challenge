f08eefb84f3787cd22649e6d6f732659b662bb2b
shaho1090 <shaho.parvini@gmail.com>
2021-09-04 12:13:17 +0430

    in order to use Here service

---
+++ app/ExternalServices/Here.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\ExternalServices;
+
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+
+class Here implements DistanceEstimatorInterface
+{
+    private string $apiKey;
+    private string $apiAddress;
+    private string $originLatLong;
+    private string $destinationLatLong;
+    private Carbon $departureTime;
+    private Carbon $arrivalTime;
+
+    public function __construct($originLatLong, $destinationLatLong)
+    {
+        $this->setConfiguration();
+        $this->originLatLong = $originLatLong;
+        $this->destinationLatLong = $destinationLatLong;
+        $this->handle();
+    }
+
+    public function setConfiguration()
+    {
+        $this->apiKey = config('here.token');
+        $this->apiAddress = config('here.address');
+    }
+
+    private function handle()
+    {
+        $query = [
+            'apiKey' => $this->apiKey,
+            'transportMode' => 'car',
+            'origin' => $this->originLatLong,
+            'destination' => $this->destinationLatLong,
+        ];
+
+        $headers = [
+            'Accept' => 'application/json'
+        ];
+
+        $response = Http::withHeaders($headers)->get(
+            $this->apiAddress,
+            $query
+        );
+
+        $response = $response->object()->routes;
+
+        $this->departureTime = Carbon::parse($response[0]->sections[0]->departure->time);
+        $this->arrivalTime = Carbon::parse($response[0]->sections[0]->arrival->time);
+    }
+
+    public function getDepartureTime(): Carbon
+    {
+        return $this->departureTime;
+    }
+
+    public function getArrivalTime(): Carbon
+    {
+        return $this->arrivalTime;
+    }
+}
