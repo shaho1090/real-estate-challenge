70cb6d11410436f3f7ef2623012681a8dd5fb768
shaho1090 <shaho.parvini@gmail.com>
2021-09-06 00:35:35 +0430

    add routes for home index and show

--- routes/home.php
+++ routes/home.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Landlord\MyHomeController;
 use App\Models\Home;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +12,7 @@ Route::middleware(['api'])->prefix('/landlord')->group(function () {
 
     Route::get('/homes', [MyHomeController::class, 'index'])
         ->name('landlord-home-index')
-        ->middleware('can:viewAny,' . Home::class);
+        ->middleware('can:viewItsOwnHomes,' . Home::class);
 
     Route::get('/homes/{home}', [MyHomeController::class, 'show'])
         ->name('landlord-home-show')
@@ -20,4 +21,14 @@ Route::middleware(['api'])->prefix('/landlord')->group(function () {
     Route::patch('/homes/{home}', [MyHomeController::class, 'update'])
         ->name('landlord-home-update')
         ->middleware('can:update,home');
+});
+
+Route::middleware(['api'])->prefix('/admin')->group(function () {
+    Route::get('/homes', [HomeController::class, 'index'])
+        ->name('homes-index')
+        ->middleware('can:viewAllHomes,' . Home::class);
+
+    Route::get('/homes/{home}', [HomeController::class, 'show'])
+        ->name('home-show')
+        ->middleware('can:viewAHomeAsAdmin,home');
 });
