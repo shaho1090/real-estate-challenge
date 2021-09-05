add55115eb7b2cc4f10c33af7e8972f3a2950c64
shaho1090 <shaho.parvini@gmail.com>
2021-09-05 11:16:40 +0430

    create collection for home

---
+++ app/Http/Resources/HomeCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class HomeCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  Request  $request
+     * @return AnonymousResourceCollection
+     */
+    public function toArray($request): AnonymousResourceCollection
+    {
+        return HomeResource::collection($this->collection);
+    }
+}
