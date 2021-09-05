0f01d6152bb8631f70142b7299236aa25b3d0abb
shaho1090 <shaho.parvini@gmail.com>
2021-09-05 11:15:50 +0430

    add index method

--- app/Http/Controllers/Landlord/MyHomeController.php
+++ app/Http/Controllers/Landlord/MyHomeController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers\Landlord;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\HomeStoreRequest;
+use App\Http\Resources\HomeCollection;
 use App\Http\Resources\HomeResource;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class MyHomeController extends Controller
 {
-    public function store(Request $request): JsonResponse
+    public function index(): JsonResponse
+    {
+        $homes = auth()->user()->homes;
+
+        return response()->json([
+            'success' => true,
+            'data' => new HomeCollection($homes)
+        ]);
+
+    }
+
+    public function store(HomeStoreRequest $request): JsonResponse
     {
        $home = auth()->user()->createHome($request->toArray());
 
