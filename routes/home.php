b87380dafc6c302b916aac87c968f7a111f58cf1
shaho1090 <shaho.parvini@gmail.com>
2021-09-05 11:24:28 +0430

    exerting policy to home routes

--- routes/home.php
+++ routes/home.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\Landlord\MyHomeController;
+use App\Models\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api'])->prefix('/landlord')->group(function () {
-    Route::post('/homes/create', [MyHomeController::class, 'store'])->name('landlord-home-create');
-    Route::get('/homes', [MyHomeController::class, 'index'])->name('landlord-home-index');
+    Route::post('/homes/create', [MyHomeController::class, 'store'])
+        ->name('landlord-home-create')
+        ->middleware('can:create,' . Home::class);
+
+    Route::get('/homes', [MyHomeController::class, 'index'])
+        ->name('landlord-home-index')
+        ->middleware('can:viewAny,' . Home::class);
 });
