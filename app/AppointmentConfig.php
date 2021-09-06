ab916971a0363a7051f9db29d0c0083d9ddf0349
shaho1090 <shaho.parvini@gmail.com>
2021-09-06 15:59:48 +0430

    appointment config for getting duration time in minutes

--- app/AppointmentConfig.php
+++ app/AppointmentConfig.php
@@ -4,7 +4,17 @@
 namespace App;
 
 
+use Carbon\Carbon;
+
 class AppointmentConfig
 {
+    /*
+     * duration in minutes
+     */
+    private const DURATION = 60;
 
+    public static function duration(): int
+    {
+        return self::DURATION;
+    }
 }
