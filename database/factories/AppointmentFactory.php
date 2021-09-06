09e8f9861b7f98163349f3baa73a5b74bf682c3a
shaho1090 <shaho.parvini@gmail.com>
2021-09-06 15:58:34 +0430

    add started and ended state for appointment factory

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -29,8 +29,40 @@ class AppointmentFactory extends Factory
             'home_id' => Home::factory()->create()->id,
             'customer_id' => User::factory()->customer()->create()->id,
             'date' => Carbon::now()->toDateTimeString(),
+            'distance_estimated_time' => rand(5,120),
             'start_time' => null,
             'end_time' => null
         ];
     }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return Factory
+     */
+    public function started(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'start_time' => Carbon::now()->toDateTimeString(),
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return Factory
+     */
+    public function ended(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'start_time' => Carbon::now()->subMinutes(75)->toDateTimeString(),
+                'end_time' => Carbon::now()->toDateTimeString(),
+            ];
+        });
+    }
+
+
 }
