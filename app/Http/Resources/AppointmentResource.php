2d46ed9d6d82e293796d30c0339bf4704959cd2f
shaho1090 <shaho.parvini@gmail.com>
2021-09-06 15:53:09 +0430

    appointment resource corrections

--- app/Http/Resources/AppointmentResource.php
+++ app/Http/Resources/AppointmentResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppointmentResource extends JsonResource
@@ -9,17 +11,20 @@ class AppointmentResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'employee' => new UserResource($this->employee),
             'home' => new HomeResource($this->home),
             'expected_date_time' => $this->date,
             'visited_start_time' => $this->start_time,
-            'visited_end_time' => $this->end_time
+            'distance_estimated_time' => $this->distance_estimated_time,
+            'probable_employee_free_time' => $this->probableEmployeeFreeTime(),
+            'visited_end_time' => $this->end_time,
         ];
     }
 }
